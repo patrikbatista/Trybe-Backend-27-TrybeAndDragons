16bae029fa07ff142d0e1e1aa35a469632c04660
patrik <pcr_batista@hotmail.com>
2022-08-11 19:48:38 -0300

    feat: implementa classe Race

---
+++ src/Race/Race.ts
@@ -0,0 +1,24 @@
+export default abstract class Race {
+  private readonly _name: string;
+  
+  private readonly _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+
+  public abstract maxLifePoints(): number;
+
+  public static createdRacesInstances(): void {
+    throw new Error('Not implemented');
+  }
+}
