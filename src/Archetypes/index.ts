3d46d8f76d0519b9ce7d4f858e552be25a8f1132
patrik <pcr_batista@hotmail.com>
2022-08-12 20:15:32 -0300

    feat: implementa Ranger no index

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -2,6 +2,7 @@ import Archetype from './Archetype';
 import Mage from './Mage';
 import Necromancer from './Necromancer';
 import Warrior from './Warrior';
+import Ranger from './Ranger';
 
 export default Archetype;
 
@@ -10,4 +11,5 @@ export {
   Mage,
   Necromancer,
   Warrior,
+  Ranger,
 };
